8c70db5629f7614726a37e50fcb1937d0fcada2f
Sazzad Rupak <Sazzad.Rupak@softability.fi>
2026-01-12 20:13:56 +0200

    Implement tweet mutation

---
+++ functions/tweet.js
@@ -0,0 +1,58 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, TransactWriteCommand } = require('@aws-sdk/lib-dynamodb');
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+const { TweetTypes } = require("../lib/constants");
+const ulid = require("ulid");
+
+const { USERS_TABLE_NAME, TWEETS_TABLE_NAME, TIMELINES_TABLE_NAME } = process.env;
+
+module.exports.handler = async (event) => {
+  const { text } = event.arguments;
+  const { username } = event.identity;
+  const id = ulid.ulid();
+  const timestamp = new Date().toJSON();
+
+  const newTweet = {
+    __typename: TweetTypes.TWEET,
+    id,
+    creator: username,
+    createdAt: timestamp,
+    text,
+    replies: 0,
+    retweets: 0,
+    likes: 0,
+  };
+
+  const input = {
+    TransactItems: [
+      {
+        Put: {
+          TableName: TWEETS_TABLE_NAME,
+          Item: newTweet,
+        },
+      }, {
+        Put: {
+          TableName: TIMELINES_TABLE_NAME,
+          Item: {
+            userId: username,
+            tweetId: id,
+            timestamp,
+          },
+        },
+      }, {
+        Update: {
+          TableName: USERS_TABLE_NAME,
+          Key: { id: username },
+          UpdateExpression: 'ADD tweetsCount :one',
+          ExpressionAttributeValues: {
+            ':one': 1
+          },
+          ConditionExpression: 'attribute_exists(id)',
+        }
+      }
+    ],
+  };
+  await docClient.send(new TransactWriteCommand(input));
+  return newTweet;
+};
